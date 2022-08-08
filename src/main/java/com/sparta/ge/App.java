b0654f6046983575807eed0d2f05e724950394b6
Georgiana <georgianaemiliou@gmail.com>
2022-08-08 15:56:59 +0100

    Design patterns

---
+++ src/main/java/com/sparta/ge/App.java
@@ -0,0 +1,24 @@
+package com.sparta.ge;
+
+
+import com.sparta.ge.factory.AnimalFactory;
+import com.sparta.ge.factory.Speakable;
+
+public class App {
+    public static void main( String[] args ) {
+        /*SingletonExample s1 = SingletonExample.getSINGLETON();
+        SingletonExample s2 = SingletonExample.getSINGLETON();
+        SingletonExample s3 = SingletonExample.getSINGLETON();
+        SingletonExample s4 = SingletonExample.getSINGLETON();
+*/
+
+
+       /* System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+        System.out.println(s4.hashCode());
+*/
+        /*Speakable dog = AnimalFactory.getAnimal("dog");
+        System.out.println(dog.getClass());*/
+    }
+}
